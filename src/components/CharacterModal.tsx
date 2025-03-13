ee4adf55e298a308d09b5dff9e2adbf6d88235cb
lucasolv505 <155476756+tridentam@users.noreply.github.com>
2025-03-13 16:39:34 -0300

    bg+

--- src/components/CharacterModal.tsx
+++ src/components/CharacterModal.tsx
@@ -1,4 +1,5 @@
 import { Character } from '../types/marvel'
+import bgapp from '../../public/images/backgroundapp.jpg'
 
 interface ModalProps {
     character: Character
@@ -12,7 +13,7 @@ function CharacterModal({ character, onClose }: ModalProps) {
     return (
         <div className="fixed inset-0 bg-black/95 flex items-center justify-center p-4 z-50" onClick={onClose}>
 
-            <div className='bg-[url(public/images/backgroundapp.jpg)] bg-cover bg-center rounded-lg max-w-2xl w-full max-h-[90vh] overflow-y-auto p-6'onClick={(e)=>e.stopPropagation()}>
+            <div style={{ backgroundImage: `url(${bgapp})` }} className='bg-cover bg-center rounded-lg max-w-2xl w-full max-h-[90vh] overflow-y-auto p-6'onClick={(e)=>e.stopPropagation()}>
 
                 <div>
 
